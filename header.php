b16e2389727cb1b7eccb9002ded47d883d19e945
Megha Agarwal <71749983+MEGHAAGARWAL1324@users.noreply.github.com>
2021-08-11 12:47:40 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>RCD THEME</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/4.css">
+    <link rel="stylesheet" media="screen and(max-width:800px)" href="<?php echo get_template_directory_uri();?>/tablet.css">
+    <link rel="stylesheet" media="screen and(max-width:530px)" href="<?php echo get_template_directory_uri();?>/phone.css">
+    <link rel="preconnect" href="<?php echo get_template_directory_uri();?>/https://fonts.googleapis.com">
+<link rel="preconnect" href="<?php echo get_template_directory_uri();?>/https://fonts.gstatic.com" crossorigin>
+<link href="<?php echo get_template_directory_uri();?>/https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Kaushan+Script&family=Merriweather:wght@300&display=swap" rel="stylesheet">
+</head>
+   <body>
+    <nav class="navbar">
+      
+    </nav>
---
+++ index.php
@@ -0,0 +1,276 @@
+
+
+<?php
+get_header();
+?>
+   <section id="home"> 
+     <div class="main">
+  <h2 class="heading">WELCOME TO <br>RC DIAMONDS</h1>
+      <button class="btn">
+        SHOP NOW
+      </button>
+    </div>
+   </section>
+     
+     <section id="Collections">
+      <h1 class="heading">OUR COLLECTIONS</h1>
+  <div class="gallery">
+    <img src="https://i.postimg.cc/52mxM4r4/pexels-dima-valkov-3266703.jpg" alt="">
+    <img src="https://i.postimg.cc/Rhv64sTy/pexels-satyam-verma-5806520.jpg" alt="">
+    <img src="https://i.postimg.cc/ZqRtVmzm/bracelet-1850-640.jpg" alt="">
+    <img src="https://i.postimg.cc/15JCxbHx/sabrinna-ringquist-tt-MO9io6u-MY-unsplash.jpg" alt="">
+    <img src="https://i.postimg.cc/Qt3kKqbk/bride-5170727-1920.jpg" alt="">
+  </div>
+  
+     </section>
+
+
+     <hr>
+     <section id="services">
+<h1 class="heading">SERVICES</h1>
+<div class="row">
+<div class="box">
+  <img src="https://i.postimg.cc/RVNDyZQZ/7581524-payment-refund-service-support-banking-icon.png" alt="">
+<h1 class="headings">REFUND </h1>
+<p><hr> the rare occasion that you are not fully satisfied with our product within 30 days of delivery, 
+  we will refund 100% of your money back or exchange it.
+   No Questions Asked!
+</p>
+</div>
+<div class="box">
+  <img src="https://i.postimg.cc/qvtHsGZQ/4544843-shop-business-delivery-trolley-comerce-icon.png" alt="">
+  <h1 class="headings">FREE DELEVERY </h1>
+  <p>
+    <hr>Yes, it's true, we ship for free. 
+    <hr>For a 100% safe purchase, all goods are insured by us until they reach you securely.
+  </p>
+  </div>
+  <div class="box">
+    <img src="https://i.postimg.cc/sxRg8NbV/7007502-document-certificate-finance-certification-business-icon.png" alt="">
+    <h1 class="headings">CERTIFIED JEWELLERY  </h1>
+    <p><strong>100% CERTIFIED JEWELLERY</strong>
+      <hr> Every piece you get is fully checked for quality and authenticity by reputed agencies:
+      
+      <hr>GOLD certified with BIS Hallmark.
+     <hr> DIAMOND certificate of authenticity from IGI, SGL and HKD.
+     <hr> SOLITAIRES carry a certificate of authenticity from GIA and IGI.
+    </p>
+    </div>
+</div>
+     </section>
+     <section id="shop">
+     <div class="small-container">
+       <h2 class="heading">SHOP  NOW</h2>
+       <div class="row">
+         <div class="col-4">
+           <img src="https://i.postimg.cc/FRj4fgHz/diamonds-4649491-1920.png" alt="">
+           <h4>Diamond ring</h4>
+           <p>35,000</p>
+           
+           <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+           </div>
+         </div>
+         <div class="col-4">
+          <img src="https://i.postimg.cc/8k6rmw45/manik-roy-SYaln4h-Cs-Ak-unsplash-1.jpg" alt="">
+          <h4>Necklace</h4>
+          <p>55,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="https://i.postimg.cc/7PmjXMZF/Whats-App-Image-2021-07-03-at-21-24-54.jpg" alt="">
+          <h4>Ring</h4>
+          <p>20,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="https://i.postimg.cc/gJ4tSmmV/diamond-3855106-1920.png" alt="">
+          <h4>Bracelet</h4>
+          <p>36,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="https://i.postimg.cc/0jbnVStv/jewellery-4255836-1920.jpg" alt="">
+          <h4>Bridal Necklace</h4>
+          <p>1,13,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="https://i.postimg.cc/FRJ163S3/pexels-say-straight-2735970-1-removebg-preview.png" alt="">
+          <h4>Diamond Earring</h4>
+          <p>15,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="https://i.postimg.cc/bNYpD1vg/sabrinna-ringquist-Eulnh2kz-UR0-unsplash.jpg" alt="">
+          <h4>Couple Rings</h4>
+          <p>55,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="https://i.postimg.cc/FRj4fgHz/diamonds-4649491-1920.png" alt="">
+          <h4>Beautiful Bracelet</h4>
+          <p>40,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="https://i.postimg.cc/RF0LJPj9/diamond-4704256-1920.png" alt="">
+          <h4>Diamond ring</h4>
+          <p>65,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="https://i.postimg.cc/bYt3x5ZN/jewellery-4721844-1920.png" alt="">
+          <h4>Diamond Bracelet</h4>
+          <p>12,000</p>
+          <div class="rating">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+            <img src="https://i.postimg.cc/DwgBBxxL/2792947-star-xmas-icon.png" alt="">
+          </div>
+        </div>
+       </div>
+     </div>
+    </section>
+     <section id="about">
+      <h2 class="heading">ABOUT US</h2>
+       <div id="pic">
+       
+         <img src="https://i.postimg.cc/FKKhfcPh/rc-logo.png" alt="">
+       <div id="intro">
+        <h2>RC DIAMOND</h2>
+        <p>Company deals in Loose Diamonds and Diamond Jewellery. 
+
+         Certified and non certified diamonds, gemstones, polki jewelry, kundan jewelry are also available.
+         
+         The jewellery are also certified.
+         
+         If you want to make diamond jewellery , we would love to discuss more in detail.<br>
+         Website: <a href="http://www.rcdiamonds.com">Website Link</a><br>Industry:Luxury Goods & Jewelry<br>
+         Company size:2-10 employees  & 46 on LinkedIn <br>
+         Includes members with current employer listed as RC Diamonds, including part-time roles.<br>
+         Headquarters:Rajkot, Gujarat<br>Type:Sole Proprietorship<br>Founded:2007<br>Specialties: Rings, Pendant, Earring, Bangles, and Necklace</p>
+      </div>
+      </div>
+    </section> 
+     <section id="contact">
+       <h1 class="heading">CONTACT US</h1>
+       <form action="" class="form">
+         <input type="text" name="name" class="input" placeholder="ENTER YOUR NAME">
+         <input type="email" name="email" class="input" placeholder="ENTER YOUR EMAIL">
+         <textarea name="msg" id="msg" cols="30" rows="10" placeholder="ENTER YOUR MESSEGE"></textarea>
+          <input type="submit" value="SEND" id="send">
+       </form>
+     </section>
+     
+   <footer>
+      <div class="main-content">
+        <div class="left box">
+          <h2>ABOUT US</h2>
+          <div class="content">
+            <p>Company deals in Loose Diamonds and Diamond Jewellery. <br>
+
+Certified and non certified diamonds, gemstones, polki jewelry, kundan jewelry are also available.<br>
+
+The jewellery are also certified.
+<br>
+If you want to make diamond jewellery , we would love to discuss more in detail.</p><br>
+            <div class="social">
+              <a href="https://facebook.com/coding.np"><span class="fab fa-facebook-f"></span></a>
+              <a href="#"><span class="fab fa-twitter"></span></a>
+              <a href="https://instagram.com/coding.np"><span class="fab fa-instagram"></span></a>
+              <a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="center box">
+          <h2>ADDRESS</h2>
+          <div class="content">
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">RAJKOT,GUJARAT</span>
+            </div>
+            <div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">+91-99301-69164 </span>
+            </div>
+            <div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text">sales@rcdiamonds.com</span>
+            </div>
+          </div>
+        </div>
+        <div class="right box">
+          <h2>CONTACT US</h2>
+          <div class="content">
+            <form action="#">
+              <div class="email">
+                <div class="text">Email *</div>
+                <input type="email" required>
+              </div>
+              <div class="msg">
+                <div class="text">Message *</div>
+
+                <textarea rows="2" cols="25" required></textarea>
+              </div>
+              <div class="btn">
+                <button type="submit">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <center>
+          <span class="credit">Created By <a href="#">RC DIAMONDS</a> | </span>
+          <span class="far fa-copyright"></span><span> 2021 All rights reserved.</span>
+        </center>
+      </div>
+    </footer>
+   
+  </body>
+</html>
